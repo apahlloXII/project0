a1461218aa2aff6d27bf0f4669ee59573b562138
apahlloXII <59705484+apahlloXII@users.noreply.github.com>
2020-01-21 10:27:57 -0800

    first commit

---
+++ sketch.js
@@ -0,0 +1,10 @@
+function setup() {
+  // put setup code here
+}
+
+function draw() {
+  // put drawing code here
+  create canvas (500,500);
+  var size = width * 0.3;
+  ellipse(width/2, height/2, size, size);
+}
