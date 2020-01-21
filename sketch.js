c60b04a66d049be7ef765831c3ca7ff35dda8da7
apahlloXII <59705484+apahlloXII@users.noreply.github.com>
2020-01-21 10:49:22 -0800

    1049 update

--- sketch.js
+++ sketch.js
@@ -4,9 +4,14 @@ function setup() {
 
 function draw() {
   // put drawing code here
+  rectMode(CENTER);
+  var size = width * 0.3;
+  rect(width, height/10, size, size);
+
   var size = width * 0.3;
   ellipse(width/2, height/2, size, size);
+
   var size = width * 0.15;
-translate(width/2, height/2);
-triangle(0, 0, size, size*2, -size, size*2);
+  translate(width/2, height/2);
+  triangle(0, 0, size, size*2, -size, size*2);
 }
