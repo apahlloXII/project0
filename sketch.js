d6b5ecdccd40ce0a905fee5f0373e6b9f567ba8e
apahlloXII <59705484+apahlloXII@users.noreply.github.com>
2020-01-21 10:47:36 -0800

    1047 update

--- sketch.js
+++ sketch.js
@@ -6,4 +6,7 @@ function draw() {
   // put drawing code here
   var size = width * 0.3;
   ellipse(width/2, height/2, size, size);
+  var size = width * 0.15;
+translate(width/2, height/2);
+triangle(0, 0, size, size*2, -size, size*2);
 }
